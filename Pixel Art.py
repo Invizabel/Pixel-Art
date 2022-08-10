e7d7af73495c286817e0c1d08ea2e15e12c7e900
Invizabel <46697708+Invizabel@users.noreply.github.com>
2022-08-10 12:36:08 -0500

    Add files via upload

---
+++ Pixel Art.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+import os
+
+files = os.listdir()
+files.sort()
+for i in files:
+    if i != "Pixel Art.py":
+        my_image = Image.open(i)
+        pixel_art = my_image.resize((16,16), Image.Resampling.BILINEAR)
+        image = pixel_art.resize((100, 100), Image.Resampling.NEAREST)
+        image.save(i)
